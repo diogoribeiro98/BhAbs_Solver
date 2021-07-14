5df52b0847fa9e314661c61281a0f50d17a0ae75
dribeiro <diogo.gnc.ribeiro@gmail.com>
2021-07-14 13:43:51 +0100

    added simple test

--- src/BhAbs_Solver.jl
+++ src/BhAbs_Solver.jl
@@ -1,5 +1,6 @@
 module BhAbs_Solver
 
 # Write your package code here.
+my_f(x,y) = x + y
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "BhAbs_Solver.jl" begin
     # Write your tests here.
+    @test BhAbs_Solver.my_f(1,1) == 2
 end
