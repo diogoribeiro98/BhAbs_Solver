06547f15185ccc78c4e87c06e72dfca73b59e056
dribeiro <diogo.gnc.ribeiro@gmail.com>
2021-07-16 14:07:23 +0100

    corrected absorption matrix

--- src/sim_utils/create_structures.jl
+++ src/sim_utils/create_structures.jl
@@ -104,6 +104,6 @@ function set_up_operator(p::Param)
     alpha!(absorption_matrix , p , Operators , 0.0)
 
     #Return final structure of operators
-    return Opps(x , y , aux_vector , Dx , Dxx , Dy , Dyy , RHS_Opp , BC_Mat , Abs_Mat , Lz)
+    return Opps(x , y , aux_vector , Dx , Dxx , Dy , Dyy , RHS_Opp , BC_Mat , absorption_matrix , Lz)
     
 end
