ea3c0b0d1fe6466b269d75e1a15bbe4c175f1a8b
dribeiro <diogo.gnc.ribeiro@gmail.com>
2021-07-17 01:54:23 +0100

    Pulse parameters are now a thing (needs work)

--- src/template_runs/binary_bh_runs.jl
+++ src/template_runs/binary_bh_runs.jl
@@ -5,7 +5,10 @@ function run_binary_BH_simulation(
         Nnodes:: Int64      ,
         Mbh::Float64        ,
         Rorbit::Float64     ,
-        alpha::Float64      , )
+        alpha::Float64      ,
+        Gaussian_pulse:: Array{Float64, 1} )
+
+
 
     #Define variables  
     Ωorbit = sqrt(2*Mbh/Rorbit^3)
@@ -19,7 +22,7 @@ function run_binary_BH_simulation(
 
     #Temporal variables
     tmax           = ttmax          , 
-    out_every_t    = 0.1            ,  
+    out_every_t    = 2.0            ,  
     deltat         = 0.05           , 
 
     #Spacial variables
@@ -33,11 +36,11 @@ function run_binary_BH_simulation(
     ynodes         =    Nnodes      , 
 
     #Initial Conditions
-    A0              =   3.5         , 
-    σ               =   2.5         , 
-    r0              =   17.0        ,
-    ω               =   0.5         , 
-    m               =   1           , 
+    A0              =   Gaussian_pulse[1] , #3.5         , 
+    σ               =   Gaussian_pulse[2] , #4.5         , 
+    r0              =   Gaussian_pulse[3] , #40.0        ,
+    ω               =   Gaussian_pulse[4] , #0.5         , 
+    m               =   Gaussian_pulse[5] , #2           , 
 
     #Klein Gordon Mass
     μ               =   0.0         ,
--- src/template_runs/single_bh_runs.jl
+++ src/template_runs/single_bh_runs.jl
@@ -5,7 +5,8 @@ function run_single_BH_simulation(
         Nnodes::Int64       , 
         Mbh::Float64        ,
         Omega::Float64      ,
-        alpha::Float64      , )
+        alpha::Float64      , 
+        Gaussian_pulse:: Array{Float64, 1} )
 
 
     #filename
@@ -30,13 +31,12 @@ function run_single_BH_simulation(
     ymax           =    Rcavity     , 
     ynodes         =    Nnodes      , 
 
-    #Initial Conditions
-    A0              =   3.5         , 
-    σ               =   1.5         , 
-    r0              =   17.0        ,
-    ω               =   0.5         , 
-    m               =   2           , 
-
+   #Initial Conditions
+   A0              =   Gaussian_pulse[1] , #3.5         , 
+   σ               =   Gaussian_pulse[2] , #4.5         , 
+   r0              =   Gaussian_pulse[3] , #40.0        ,
+   ω               =   Gaussian_pulse[4] , #0.5         , 
+   m               =   Gaussian_pulse[5] , #2           , 
     #Klein Gordon Mass
     μ               =   0.0         ,
 
