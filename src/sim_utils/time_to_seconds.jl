c6a58abaaf83ca0dd9943c12fd303d8699700cee
dribeiro <diogo.gnc.ribeiro@gmail.com>
2021-07-22 01:22:47 +0100

    added seconds to string (hh:mm:ss ) function

--- src/sim_utils/time_to_seconds.jl
+++ src/sim_utils/time_to_seconds.jl
@@ -18,3 +18,16 @@ function time_string_to_seconds( time::String)
     return t_seconds
 end
 
+#String to time
+function time_seconds_to_string( seconds::Float64)
+
+    #
+    hour = round( Int, seconds / (60*60))
+    seconds %= (60*60)
+    minutes = round(Int , seconds / 60, ) 
+    seconds %= 60
+    
+    return string(  lpad(hour,2,"0") , ":" ,lpad(minutes,2,"0") , ":" , lpad(round(Int , seconds),2,"0") )
+
+end
+
