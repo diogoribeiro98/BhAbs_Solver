f7215d5514dde73e8d49d503d91d489b1bd5dbe8
dribeiro <diogo.gnc.ribeiro@gmail.com>
2021-07-26 20:18:00 +0100

    update template test file

--- test/template_test_file.jl
+++ test/template_test_file.jl
@@ -1,8 +1,22 @@
 using BhAbs_Solver
-using Test
 
-#@testset "BhAbs_Solver.jl" begin
-  
-  @test BhAbs_Solver.run_single_BH_simulation( 1. ) == 1
+run_binary_BH_simulation(
 
-#end
+  #Time variables
+  tspan   =   [0. , 10.]   ,
+  dt                =   0.05        ,
+  out_every         =   1.0         ,
+  max_run_time        =   "24:00:00"  ,
+
+  #Spatial variables
+  Nnodes              =   128     ,
+  Rcavity            =   40.     ,
+
+  #Black hole parameters
+  Mbh                =   1.0     ,
+  Rorbit             =   6.0     ,
+  alpha              =   10.0    ,
+
+  #Initial configuration
+  Gaussian_pulse = [ 3.5 , 3.7 , 20.  , 0.1 , 2 ],
+  )
