6e69611c6a5bfb17744bcf9bdf858de0c03db105
dribeiro <diogo.gnc.ribeiro@gmail.com>
2021-07-26 17:47:03 +0100

    Function to print goodbye message and timetaken

---
+++ src/sim_utils/end_message.jl
@@ -0,0 +1,11 @@
+function print_output_message(time_taken::Float64)
+
+        println("Finished!")      
+        println(" ")
+        println("Thank you for choosing KG Solver!")
+        println("___________________________________________________________")  
+        print("Time Taken:  " , time_taken )
+     
+        return 
+end
+        
